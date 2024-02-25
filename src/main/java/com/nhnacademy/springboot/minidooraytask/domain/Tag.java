55859638fb9198fc4f1d003f77b058f5e5a7cef8
{qaw302} <{osshun21@gmail.com}>
2024-02-25 23:56:33 +0900

    fix : tag 수정

--- src/main/java/com/nhnacademy/springboot/minidooraytask/domain/Tag.java
+++ src/main/java/com/nhnacademy/springboot/minidooraytask/domain/Tag.java
@@ -9,6 +9,7 @@ import javax.persistence.*;
 public class Tag {
     @Id
     @Column(name = "tag_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long tagId;
 
     @ManyToOne
--- src/main/java/com/nhnacademy/springboot/minidooraytask/domain/dto/TagName.java
+++ src/main/java/com/nhnacademy/springboot/minidooraytask/domain/dto/TagName.java
@@ -2,8 +2,10 @@ package com.nhnacademy.springboot.minidooraytask.domain.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class TagName {
     private String tagName;
